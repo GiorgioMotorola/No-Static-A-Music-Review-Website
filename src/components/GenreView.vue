bc259eb7404d12c199e3b2657ff659c4e0c8aafb
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-05-01 22:22:44 -0400

    added review

--- src/components/GenreView.vue
+++ src/components/GenreView.vue
@@ -25,6 +25,7 @@ export default {
                 "Power Pop",
                 "Punk",
                 "Rock",
+                "Sophisti-Pop",
                 "Yacht Rock"
             ]
         };
