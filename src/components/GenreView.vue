d688b0aa233e5f19843474c70f5172c363292893
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-05-07 20:25:50 -0400

    added review

--- src/components/GenreView.vue
+++ src/components/GenreView.vue
@@ -19,6 +19,7 @@ export default {
                 "Country",
                 "Electronic",
                 "Hip Hop",
+                "Indie Pop",
                 "Indie Rock",
                 "Jazz",
                 "Metal",
