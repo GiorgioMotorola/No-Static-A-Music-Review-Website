540dc4a522055639dee7d68ef725c177fde6c11a
Mike Weatherford <mikesweatherford@gmail.com>
2025-03-15 15:31:01 -0400

    box shawdow disabled for mobile again again

--- src/components/DefSpringList.vue
+++ src/components/DefSpringList.vue
@@ -524,7 +524,7 @@ button {
     max-width: 220px;
   }
   .calendar-day:hover {
-  box-shadow: white 0px 0px 0px;
+    box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
 }
   .header-current-month {
     font-size: 17px;
